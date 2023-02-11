9f7f3a5cc442b9218413f78e034a3f0ec5f14772
Kiverok <kiver566@ukr.net>
2023-02-11 13:35:22 +0200

    index

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import './index.css';
 import App from './App';
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
     <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    </BrowserRouter> 
+  </React.StrictMode>
 );
 
 
