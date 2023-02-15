0b2b518632f6ea45275bc8433a46afa0979366b8
Kiverok <kiver566@ukr.net>
2023-02-15 17:33:07 +0200

    form

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux"
+import form from "./form"
+import { setUser } from "../store/slices/userSlice";
+
+const login = () => {
+    const dispatch = useDispatch();
+
+    const handleLogin = () => {
+
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default login
